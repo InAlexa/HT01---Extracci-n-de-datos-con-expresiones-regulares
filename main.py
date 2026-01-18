854963ec221e874df9c34b8b9b4667dee3700e17
Daniela Marin <danie0le2@gmail.com>
2026-01-18 17:13:43 -0600

    Arreglar el main.py

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ import pandas as pd
 import re
 from PyPDF2 import PdfReader
 
-rutaArch = "C:\\Users\\danie\\OlsneDrive\\Escritorio\\compiladores\\Analisis Anual 2018 ETAS.pdf"
+rutaArch = "C:\\Users\\danie\\OneDrive\\Escritorio\\compiladores\\Analisis Anual 2018 ETAS.pdf"
 documento = PdfReader(rutaArch)
 x=len(documento.pages)
 pagina = documento.pages[4]
 texto = pagina.extract_text()
 print(texto)
 
-"""
-patron = r'\n(\w+\s*\w+\s*\w*)\s+(\d+)\s+(\d+.\d+)\s+(\d+)\s+(\d+.\d+)'
+patron = r'\n(\d+){1,2}\s{1}(\d+){1,2}\s{1}([\s\S]*?(?=  |\d))(?:\s{2})(.*?(?=  ))(?:\s{2})(.*?(?=\d))(\d{1,})'
 
 datos = re.findall(patron, texto)
 print(datos)
 
-etas = pd.DataFrame(datos, columns = ['No.', 'SE','Área de Salud, 'ETA reportada', 'Tasas 2018'])
-print(etas)"""
+etas = pd.DataFrame(datos, columns = ['No.', 'SE', 'Área de Salud', 'ETA reportada', 'Fuente de Contagio', 'Número de Casos'])
+print(etas)
